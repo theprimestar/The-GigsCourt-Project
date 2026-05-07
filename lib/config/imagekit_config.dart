bad085329c4b99f267c2f23b77d7a09e6800637d
Gigscourt <theprimestarventures@gmail.com>
2026-05-07 09:35:02 +0100

    Create imagekit_config.dart

---
+++ lib/config/imagekit_config.dart
@@ -0,0 +1,6 @@
+class ImageKitConfig {
+  static const String publicKey = 'public_hwM9hldZI+DqFY/pncPQCA5VRWo=';
+  static const String urlEndpoint = 'https://ik.imagekit.io/Theprimestar';
+  static const String authEndpoint =
+      'https://paohowngwtpffbdxxije.supabase.co/functions/v1/imagekit-auth';
+}
